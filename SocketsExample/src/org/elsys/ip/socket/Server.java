1dbc1fada3c1c958c462633fed1e422405fbb438
Julian Stoev <xxthespinerxx@gmail.com>
2016-10-29 18:20:53 +0300

    Added unit Servlet code

---
+++ SocketsExample/src/org/elsys/ip/socket/Server.java
@@ -0,0 +1,29 @@
+package org.elsys.ip.socket;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+	private ServerSocket serverSocket;
+	
+	public Server(int port) throws IOException {
+		serverSocket = new ServerSocket(port);
+	}
+	
+	public void start() throws IOException {
+		Socket client = serverSocket.accept();
+		
+		PrintStream printStream = new PrintStream(client.getOutputStream());
+		
+		Scanner scanner = new Scanner(client.getInputStream());
+		
+		while (scanner.hasNext()) {
+			String nextLine = scanner.nextLine();
+			System.out.println(nextLine);
+		}
+	}
+
+}
